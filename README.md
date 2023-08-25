4b648513acf27eeda6d2e033412604fddadca03a
Jay Pfaffman <jay@literatecomputing.com>
2023-08-25 16:15:21 -0500

    first real commit

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-# **Theme Name**
+# Add QR Code
 
-**Theme Summary**
+Adds a line to the composer pull-down that will insert:
 
-For more information, please see: **url to meta topic**
+```
+[wrap=qrcode url=]
+[/wrap]
+```
+and will render a QR Code. If you leave `url=` blank, it will use the current page URL. You can also type whatever text you want in after `url=` to have that text rendered in the QR Code (tip: It doesn't have to be a URL, but does need to be enclosed in "quotes").
+
+It seems that due to limitations of the library you can have only one QR Code per post.
+
+Uses code from https://davidshimjs.github.io/qrcodejs/
---
+++ javascripts/discourse/api-initializers/api-setup.js
@@ -0,0 +1,76 @@
+import { apiInitializer } from "discourse/lib/api";
+import loadScript from "discourse/lib/load-script";
+import I18n from "I18n";
+
+async function applyQrcode(element, key = "composer") {
+  const qrcodes = element.querySelectorAll(".d-wrap[data-wrap=qrcode]");
+  if (!qrcodes.length) {
+    return;
+  }
+
+  await loadScript(settings.theme_uploads_local.qrcode_js);
+
+  qrcodes.forEach((qrcode) => {
+    const qr = document.createElement("div");
+    qr.classList.add("qrcode");
+    const url = qrcode.getAttribute("data-url")
+      ? qrcode.getAttribute("data-url")
+      : window.location.href;
+    new window.QRCode(qr, url);
+    let node = document.createTextNode(url);
+
+    qrcode.appendChild(node);
+    qrcode.append(qr);
+  });
+}
+
+export default apiInitializer("0.11.1", (api) => {
+  const { iconNode } = require("discourse-common/lib/icon-library");
+  let qrcode_icon = iconNode("qrcode");
+
+  const currentLocale = I18n.currentLocale();
+  // I18n.translations[currentLocale].js.qrcode_button_title = I18n.t(themePrefix("composer_footnote_button_title"));
+  // I18n.translations[currentLocale].js.composer.qrcode_button_text = I18n.t(themePrefix("composer_footnote_button_text"));
+  I18n.translations[currentLocale].js.qrcode_button_title = "Insert QR Code";
+  I18n.translations[currentLocale].js.composer.qrcode_button_text =
+    "Insert QR Code";
+
+  api.modifyClass("controller:composer", {
+    pluginId: "qrcode",
+
+    actions: {
+      qrcodeButton() {
+        this.get("toolbarEvent").addText(
+          "\n" + `[wrap=qrcode url=]` + "\n[/wrap]\n"
+        );
+      },
+    },
+  });
+
+  api.addToolbarPopupMenuOptionsCallback((controller) => {
+    return {
+      icon: "qrcode",
+      label: "qrcode_button_title",
+      action: "qrcodeButton",
+    };
+  });
+
+  // add button rather than to the menu
+  // api.onToolbarCreate((toolbar) => {
+  //   toolbar.addButton({
+  //     id: "composer_qrcode",
+  //     group: "extras",
+  //     icon: "qrcode",
+  //     title: "qrcode_button_text",
+  //     perform: (e) => e.addText("\n" + `[wrap=qrcode url=]` + "\n[/wrap]\n"),
+  //   });
+  // });
+
+  api.decorateCookedElement(
+    async (elem, helper) => {
+      const id = helper ? `post_${helper.getModel().id}` : "composer";
+      applyQrcode(elem, id);
+    },
+    { id: "discourse-qrcode-theme-component" }
+  );
+});
